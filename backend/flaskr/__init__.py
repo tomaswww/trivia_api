cbc2a91985831da5c0ab812db046c55a4abd9887
tomaswww <twingord@gmail.com>
2019-10-12 02:47:04 -0300

    added comment

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -41,7 +41,7 @@ def create_app(test_config=None):
       'total_categories':len(fixed_categories)
     })
 
-  #@TODO: Create an endpoint to handle GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, number of total questions, current category, categories. 
+  #@TODO: Create an endpoint to handle GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, number of total questions, current category, categories.--> DONE
   #TEST: At this point, when you start the application you should see questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers should update the questions.
   @app.route('/questions', methods=['GET'])
   def get_questions():
