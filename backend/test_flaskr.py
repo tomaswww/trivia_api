80bd676f52920349b24d78e5fd0b9a129c2fd29e
tomaswww <twingord@gmail.com>
2019-10-12 03:19:10 -0300

    Added unittest functions for testing endpoints

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -32,7 +32,109 @@ class TriviaTestCase(unittest.TestCase):
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
+
+    test style:
+    def test_given_behavior(self):
+        res = self.client().get('/')
+
+        self.assertEqual(res.status_code, 200)
+
     """
+    def test_get_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['categories'])
+        self.assertTrue(data['total_categories'])
+
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['categories']))
+
+    def test_get_questions_invalid_page(self):
+        res = self.client().get('/questions?page=9999')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+        
+    #not sure if this one should fail or not
+    def test_delete_question(self):
+        res = self.client().delete('questions/1')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['deleted'])
+        self.assertTrue(data['total_questions'])
+
+    def test_delete_question_invalid(self):
+        res = self.client().delete('questions/9999')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_post_questions(self):
+        new_question={
+            'question': 'success testing',
+            'answer': 'succes answer',
+            'category': 1,
+            'difficulty': '1'
+        }
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+    
+    def test_post_questions_invalid(self):
+        new_question = {
+            'question': 'invalid testing',
+            'answer': '',
+            'category': 1,
+            'difficulty': ''
+        }
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+    
+    def test_search_question(self):
+        res = self.client().post('/questions/search',json={'searchTerm': "title"})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_search_question_invalid(self):
+        res = self.client().post('/questions/search',json={'searchTerm': "invalidTermRequest"})
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    def test_get_questions_by_category(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(data['total_questions'])
+
+    def test_get_questions_by_category_invalid(self):
+        res = self.client().get('/categories/999/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')    
+
 
 
 # Make the tests conveniently executable
