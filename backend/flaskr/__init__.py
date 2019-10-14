ed3e1712e3b2d7d8b8daf25d8476d204b00d65ef
tomaswww <twingord@gmail.com>
2019-10-14 01:32:46 -0300

    Added DONE comments to the code

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -43,7 +43,7 @@ def create_app(test_config=None):
     })
 
   #@TODO: Create an endpoint to handle GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, number of total questions, current category, categories.--> DONE
-  #TEST: At this point, when you start the application you should see questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers should update the questions.
+  #TEST: At this point, when you start the application you should see questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers should update the questions. -->DONE
   @app.route('/questions', methods=['GET'])
   def get_questions():
     questions = Question.query.all()
@@ -64,7 +64,7 @@ def create_app(test_config=None):
     })
   
   #@TODO: Create an endpoint to DELETE question using a question ID. --> DONE
-  #TEST: When you click the trash icon next to a question, the question will be removed. This removal will persist in the database and when you refresh the page.
+  #TEST: When you click the trash icon next to a question, the question will be removed. This removal will persist in the database and when you refresh the page. -->DONE
   @app.route('/questions/<int:id>', methods=['DELETE'])
   def delete_question(id):
     try:
@@ -84,7 +84,7 @@ def create_app(test_config=None):
       abort(422)
 
   #@TODO:Create an endpoint to POST a new question, which will require the question and answer text, category, and difficulty score.--> DONE
-  #TEST: When you submit a question on the "Add" tab, the form will clear and the question will appear at the end of the last page of the questions list in the "List" tab.
+  #TEST: When you submit a question on the "Add" tab, the form will clear and the question will appear at the end of the last page of the questions list in the "List" tab.--> DONE
   @app.route('/questions', methods=['POST'])
   def create_question():
     data = request.get_json()
@@ -107,7 +107,7 @@ def create_app(test_config=None):
       abort(422)
 
   #@TODO: Create a POST endpoint to get questions based on a search term. It should return any questions for whom the search term is a substring of the question. --> DONE
-  #TEST: Search by any phrase. The questions list will update to include only question that include that string within their question. Try using the word "title" to start.
+  #TEST: Search by any phrase. The questions list will update to include only question that include that string within their question. Try using the word "title" to start.--> DONE
   @app.route('/questions/search', methods=['POST'])
   def search_questions():
     data = request.get_json()
@@ -129,7 +129,7 @@ def create_app(test_config=None):
       abort(422)
 
   #@TODO: Create a GET endpoint to get questions based on category. --> DONE
-  #TEST: In the "List" tab / main screen, clicking on one of the categories in the left column will cause only questions of that category to be shown. 
+  #TEST: In the "List" tab / main screen, clicking on one of the categories in the left column will cause only questions of that category to be shown. -->DONE
   @app.route('/categories/<int:id>/questions', methods=['GET'])
   def category_questions(id):
     category_id = str(id)
@@ -153,7 +153,7 @@ def create_app(test_config=None):
    
 
   #@TODO: Create a POST endpoint to get questions to play the quiz. This endpoint should take category and previous question parameters and return a random questions within the given category,if provided, and that is not one of the previous questions. --> DONE
-  #TEST: In the "Play" tab, after a user selects "All" or a category,one question at a time is displayed, the user is allowed to answer and shown whether they were correct or not. 
+  #TEST: In the "Play" tab, after a user selects "All" or a category,one question at a time is displayed, the user is allowed to answer and shown whether they were correct or not. -->DONE
   @app.route('/quizzes', methods=['POST'])
   def play():
     #DATA returned from the FE
