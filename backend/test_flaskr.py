3cbf1cbf5effb594db8cf3e96f071e454bcc937d
tomaswww <twingord@gmail.com>
2019-10-15 09:54:32 -0300

    Fixed test:flask.py to adhere to PEP 8 style guide

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('tomaswingord:tomasw87@localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(\
+            'tomaswingord:tomasw87@localhost:5432', \
+            self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,14 +26,13 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    
-    #TODO: Write at least one test for each test for successful operation and for expected errors. -- DONE
-    
+    # TODO: Write at least one test for each test for successful \ 
+    # operation and for expected errors. -- DONE
     def test_get_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
@@ -56,8 +57,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'Not found')
-        
-    #not sure if this one should fail or not
+
+    # not sure if this one should fail or not
     def test_delete_question(self):
         res = self.client().delete('questions/2')
         data = json.loads(res.data)
@@ -74,7 +75,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Not found')
 
     def test_post_questions(self):
-        new_question={
+        new_question = {
             'question': 'success testing',
             'answer': 'succes answer',
             'category': 1,
@@ -85,7 +86,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_questions'])
-    
+
     def test_post_questions_invalid(self):
         new_question = {
             'question': 'invalid testing',
@@ -97,9 +98,10 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 422)
         self.assertEqual(data['success'], False)
-    
+
     def test_search_question(self):
-        res = self.client().post('/questions/search',json={'search_term': "title"})
+        res = self.client().post('/questions/search', \
+            json={'search_term': "title"})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
@@ -107,7 +109,8 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
 
     def test_search_question_invalid(self):
-        res = self.client().post('/questions/search',json={'search_term': "doesnotexist"})
+        res = self.client().post('/questions/search', \
+            json={'search_term': "doesnotexist"})
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
@@ -126,9 +129,7 @@ class TriviaTestCase(unittest.TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
-        self.assertEqual(data['message'], 'Not found')    
-
-
+        self.assertEqual(data['message'], 'Not found')  
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
